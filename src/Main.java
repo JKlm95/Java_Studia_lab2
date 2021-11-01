b1310c1941b148df9082ce263861ccfeb6eb9de4
Jakub <j.klimiuk95@gmail.com>
2021-11-01 19:32:30 +0100

    Cw 3. Zad 1. Zad 2.

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,78 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+//        Scanner scan = new Scanner((System.in));
+//        System.out.print("Podaj liczbę: ");
+//        int a = Integer.parseInt(scan.nextLine());
+//
+//        switch (a % 2) {
+//            case 0:
+//                System.out.println("Liczba podzielna przez 2");
+//                break;
+//            default:
+//                System.out.println("Liczba nie podzielna przez 2");
+//                break;
+//        }
+
+
+
+//        Scanner scan = new Scanner((System.in));
+//        System.out.print("Podaj liczbę: ");
+//        int a = Integer.parseInt(scan.nextLine());
+//
+//        switch (a % 7) {
+//            case 0:
+//                System.out.println("Liczba podzielna przez 7");
+//                break;
+//            default:
+//                System.out.println("Liczba nie podzielna przez 7");
+//                break;
+//        }
+
+
+
+
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+
+
+//        String s = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+//        int[] tab = {0, 0, 0, 0, 0, 0};
+//
+//
+//        for (char zmiennaChar : s.toCharArray())
+//        {
+//            switch (zmiennaChar) {
+//                case 'o':
+//                    ++tab[0];
+//                    break;
+//
+//                case 'p':
+//                    ++tab[1];
+//                    break;
+//
+//                case 'x':
+//                    ++tab[2];
+//                    break;
+//                case 'z':
+//                    ++tab[3];
+//                    break;
+//                case 'q':
+//                    ++tab[4];
+//                    break;
+//                default:
+//                    ++tab[5];
+//                    break;
+//            }
+//        }
+//                System.out.printf("\nLitera występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy\ninne: %d - razy",
+//                        tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]);
+
+
+
 
 
     }
