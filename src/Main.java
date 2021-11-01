ebcc27ab8bbca4ad97e7ab8233a977cff9ca7ccc
Jakub <j.klimiuk95@gmail.com>
2021-11-01 19:55:37 +0100

    Cw 4. Zad 1.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,46 @@ public class Main {
     public static void main(String[] args) {
 
         /* cw.4  */
+        //zad.1 napisać 3 pętle for i użyc:
+       // a) w pierwszej pętli break
 
-        /* zad.1 napisać 3 pętle for i użyc:
-        a) w pierwszej pętli break
-        b) w drugiej pętli continue
-        c) w trzeciej pętli return
-        */
+        //Wypisuje liczby od 0 do 100, gdy dojdzie do 50 (nie wypisuje 50) przerywa i wychodzi z pętli.
+
+//        for (int i = 0; i <= 100; i++) {
+//            if (i == 50)
+//                break;
+//            System.out.print(i + " ");
+//        }
+//
+//        System.out.println("\n");
+
+
+
+
+      //  b) w drugiej pętli continue
+
+        //Wypisuje liczby od 0 do 10 z pominięciem tych, które są podzielne przez 2.
+
+//        for (int i = 0; i <= 10; i++) {
+//            if (i % 2 == 0)
+//                continue;
+//            System.out.print(i + " ");
+//        }
+//
+//        System.out.println("\n");
+
+
+       // c) w trzeciej pętli return
+
+        //Wypisuje liczby od 0 do 100, gdy dojdzie do 50 (nie wypisuje 50) przerywa.
+
+
+//        for (int i = 0; i <= 100; i++) {
+//            if (i == 50)
+//                return;
+//            System.out.print(i + " ");
+//        }
+//        System.out.println("\n");
 
 
     }
