9869df12b03dbab4bfdf7bd4ca106f514b0e8694
Jakub <j.klimiuk95@gmail.com>
2021-11-01 15:09:02 +0100

    Cw 2.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* cw.1 pętle while */
 
-        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
-          Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
-          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
-        */
         /* cw.1 pętle while */
 
 /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
@@ -15,17 +10,17 @@ public class Main {
   to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
 */
 
-        int i = 1000;
-        int index = 23447;
-        int lastTwoIndex = index % 100;
-
-        do {
-            if (i % lastTwoIndex == 0) {
-                System.out.println("liczba " + i + " jest podzielna przez " + lastTwoIndex);
-            }
-            --i;
-        }
-        while (i > 0);
+//        int i = 1000;
+//        int index = 23447;
+//        int lastTwoIndex = index % 100;
+//
+//        do {
+//            if (i % lastTwoIndex == 0) {
+//                System.out.println("liczba " + i + " jest podzielna przez " + lastTwoIndex);
+//            }
+//            --i;
+//        }
+//        while (i > 0);
 
 
         /* zad 2
@@ -35,10 +30,48 @@ public class Main {
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
 
+
+//        int[] tab = {1,4,3,5,8};
+//        int index = 0;
+//        while (index < tab.length)
+//        {
+//            System.out.println(tab[index]);
+//            index++;
+//        }
+
+
+
+
+//        Double[] tab = {1.3,5.2,6.0,6.6,9.1};
+//        int index = 0;
+//        while (index < tab.length)
+//        {
+//            System.out.println(tab[index]);
+//            index++;
+//        }
+
+
+
+//        String[] days = {"Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"};
+//        int index = 0;
+//        while (index < days.length)
+//        {
+//            System.out.println(days[index]);
+//            index++;
+//        }
+
+
+
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
+
+
+
+
+
+
 
 
     }
